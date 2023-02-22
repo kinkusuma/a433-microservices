d0fa6b41e14b24c9618d97ca3a319ee495c76723
Kinie K Kusuma <66422665+kinkusuma@users.noreply.github.com>
2023-02-22 15:49:55 +0800

    Create build_push_image_karsajobs_ui.sh

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# build docker image
+docker build -t karsajob-uis:latest .
+
+# menyesuaikan nama image sesuai format registry
+docker tag karsajobs-ui:latest ghcr.io/kinkusuma/karsajobs-ui:latest
+
+# login ke registry
+echo $CR_PAT | docker login ghcr.io -u kinkusuma --password-stdin
+
+# push image ke registry
+docker push ghcr.io/kinkusuma/karsajobs-ui:latest
